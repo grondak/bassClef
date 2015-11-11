db1623fb6ea8af8dedb24ab72dfb5fe6f70d2a28
grondak <grondak@gmail.com>
2015-11-11 10:43:43 -0500

    Adding istanbul test code coverage in build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     jslint = require('gulp-jslint'),
     mocha = require('gulp-mocha'),
     files = ['lib/index.js', 'test/test.js', 'spec/music-spec.js'],
-    jasmine = require('gulp-jasmine');
+    jasmine = require('gulp-jasmine'),
+    istanbul = require('gulp-istanbul');
 
 // npm install gulp-jslint
 gulp.task('jslint', function() {
@@ -72,5 +73,22 @@ gulp.task('jasmine', ['mocha'], function () {
         .pipe(jasmine());
 });
 
+gulp.task('pre-coverage', ['jasmine'], function () {
+  return gulp.src(['lib/**/*.js'])
+    // Covering files 
+    .pipe(istanbul())
+    // Force `require` to return covered files 
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('coverage', ['pre-coverage'], function () {
+  return gulp.src(['test/*.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran 
+    .pipe(istanbul.writeReports())
+    // Enforce a coverage of at least 90% 
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+});
+
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['jslint', 'mocha', 'jasmine']);
+gulp.task('default', ['jslint', 'mocha', 'jasmine', 'coverage']);
